58126722ec2faf692b2e997b4008c5bf1bc1fc98
Pedro <nebewu@gmail.com>
2022-05-28 00:18:23 -0300

    Implement User model migration and validation

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  validates :name, presence: true, uniqueness: true, length: { in: 1..20 }
+  validates :password, presence: true, length: { in: 8..32 }
+  validates :email, presence: true, uniqueness: true, format: { with: /.*@.*/ }
+end
