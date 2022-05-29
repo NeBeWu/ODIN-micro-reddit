4c62a862143d6fbd3763a4566fab3a64eb19aaa4
Pedro <nebewu@gmail.com>
2022-05-29 14:11:40 -0300

    Add user_id presence validation to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,5 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { in: 1..40 }
   validates :link, presence: true, if: -> { body.blank? }
   validates :body, presence: true, if: -> { link.blank? }
+  validates :user_id, presence: true
 end
